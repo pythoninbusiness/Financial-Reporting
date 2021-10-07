4541282eb88297222c3f0a67ce1b2ef3a66594da
pythoninbusiness <90924851+pythoninbusiness@users.noreply.github.com>
2021-10-07 13:44:56 -0700

    Uploaded Regex Tie Out script

---
+++ Tie Out Financial Statements with RegEx/Tie Out HTML Filing.py
@@ -0,0 +1,52 @@
+"""
+Purpose: To take a an HTML financial document (i.e., 10-Q) and tie out all numbers found in paragraphs. For example, $8 million or 5,676,392 shares. 
+The script uses regular expressions, so depending on how the Company writes their disclosures and reports, one may need to update the regular expression to catch all. 
+
+The output file is an HTML file which can then be printed into a PDF and further tied out.
+"""
+
+
+import re
+
+file_path = r".\Example 10-Q.htm"
+
+with open(file_path, 'r') as file_:
+    html_text = file_.read()
+
+html_text = re.sub("&#160;", " ", html_text)
+html_text = re.sub(r"(\$[\d,\.]*) (million|billion|thousand)*", "<span class='tie-out'>" + r"\g<0>" + "</span>", html_text)
+html_text = re.sub(r"([\d,\.]+ shares)", "<span class='tie-out'>" + r"\g<0>" + "</span>", html_text)
+html_text = re.sub(r"(\d+,\d\d\d[,\d\d\d]* \w+)", "<span class='tie-out'>" + r"\g<0>" + "</span>", html_text)
+html_text = re.sub(r"\s([\d\.\,]+\%)", " <span class='tie-out'>" + r"\g<1>" + "</span>", html_text)
+
+output = f"""
+<html>
+    <head>
+        <style>
+        .tie-out {{
+        border: 2px solid red;
+        padding: 1px;
+        }}
+	.hide-tie-out {{
+            border: none !important;
+            position: unset;
+        }}
+        </style>
+    </head>
+
+    <body>
+    {html_text}
+
+     <script>
+        const tieOutValues = document.querySelectorAll('.tie-out');
+
+        tieOutValues.forEach(el => el.addEventListener('click', event => {{
+            el.classList.toggle("hide-tie-out");
+        }}));
+    </script>
+    </body>
+</html>
+"""
+
+with open("Tied Out Report.html", 'w', encoding="utf-8") as file_:
+    file_.write(output)
